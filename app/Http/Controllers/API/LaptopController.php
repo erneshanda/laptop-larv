3c8e6a0f246206b20878246f12664188dd6280dd
erneshanda <ernes.422023031@civitas.ukrida.ac.id>
2025-07-01 21:25:31 +0700

    fix

---
+++ app/Http/Controllers/API/LaptopController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use App\Models\Laptop;
+use function PHPUnit\Framework\returnArgument;
+
+class LaptopController extends Controller
+{
+    public function index()
+    {
+        $laptops = Laptop::all();
+        return response()->json(array('message'=>'Data retrieved succesfully', 'data'=>$laptops), 200);
+    }
+
+    public function store(Request $request) 
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|unique:laptops',
+                'price' => 'required|min:0',
+            ]);
+
+            if ($validator->fails()) {
+                throw new HttpException(400, $validator->messages()->first());
+            }
+
+            $laptop = new Laptop();
+            $laptop->fill($request->all())->save();
+
+            return response()->json(array('message'=>'Saved successfully', 'data'=>$laptop), 200);
+        } catch (\Exception $exception) {
+            throw new HttpException(400, "Invalid data - {$exception->getMessage()}");
+        }
+    }
+
+    public function show($id) 
+    {
+        $laptop = Laptop::findOrFail($id);
+        return response()->json(array('message'=>'Saved successfully', 'data'=>$laptop), 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        if (!$id) {
+            throw new HttpException(400, 'Invalid ID');
+        }
+
+        $laptop = Laptop::find($id);
+        if(!$laptop) {
+            throw new HttpException(400, 'Laptop not found');
+        }
+
+        try {
+            $laptop->fill($request->all())->save();
+            return response()->json(array('message'=>'Updated successfully', 'data'=>$laptop), 200);
+        }
+    } 
+
+    public function destroy($id)
+    {
+        $laptop = Laptop::findOrFail($id);
+        $laptop->delete();
+
+        return response()->json(array('message'=>'Deleted successfully', 'data'=>$laptop), 204);   
+    }
+}
---
+++ database/migrations/2025_07_01_132659_create_laptops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laptops', function (Blueprint $table) {
+            $table->id();
+            $table->enum('brand', ['Asus', 'Lenovo', 'Dell', 'MSI', 'HP', 'Xiaomi']);
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->double('price');
+            $table->string('image')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->integer('deleted_by')->nullable();
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laptops');
+    }
+};
